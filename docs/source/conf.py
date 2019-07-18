2e65a5cbf1ae7afbd0cf14e2a55bfee095f21155
ankush13r <51945739+ankush13r@users.noreply.github.com>
2019-07-18 20:33:56 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../../2project_PlanTL/'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
